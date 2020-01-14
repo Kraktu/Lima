6c22941a160f1f9f2f43f07c61c3982035f5af04
Strauss16 <normanandry@hotmail.be>
2020-01-14 14:46:12 +0100

    resource et resource manager correction

---
+++ Assets/Scripts/Resource.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resource : MonoBehaviour
+{
+	public string resourceName;
+	public double totalResource = 0;
+	public double resourcePerSecond = 1;
+	public double resourcePerClick = 1;
+
+}
---
+++ Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+
+{
+	public Resource wood = new Resource { resourceName = "wood"};
+	public Resource ore = new Resource { resourceName = "ore" };
+	public Resource venacid = new Resource{ resourceName = "venacid" };
+	public int startingWood, startingOre, startingVenacid;
+	public int startingWoodPerSecond, startingOrePerSecond, startingVenacidPerSecond;
+	public int startingWoodPerClick, startingOrePerClick, startingVenacidPerClick;
+
+	private void Start()
+	{
+		wood.totalResource = startingWood;
+		ore.totalResource = startingOre;
+		venacid.totalResource = startingVenacid;
+
+		wood.resourcePerSecond = startingWoodPerSecond;
+		ore.resourcePerSecond = startingOrePerSecond;
+		venacid.resourcePerSecond = startingVenacidPerSecond;
+
+		wood.resourcePerClick = startingWoodPerClick;
+		ore.resourcePerClick = startingOrePerClick;
+		venacid.resourcePerClick = startingVenacidPerClick;
+	}
+
+}
