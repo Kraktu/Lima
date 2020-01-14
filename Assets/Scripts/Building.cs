593d5defdffb48fe342de1675771d279cd3e5da0
Kraktu <dupontphilip15@gmail.com>
2020-01-14 14:39:47 +0100

    Building+buildingManager creation

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    public string buildingName;
+    public int level = 0;
+    public Vector3Int cost;
+    public Vector3Int upgradeCost;
+    public Vector3 CostMultiplicator;
+    public bool canBuild=false;
+
+}
---
+++ Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour
+{
+    public Building sawmill, mine, headquarter, incubator;
+    public string sawmillName, mineName, headquarterName, incubatorName;
+    public Vector3Int sawmillCost, mineCost, headquarterCost, incubatorCost;
+    public Vector3Int sawmillUpgradeCost, mineUpgradeCost, headquarterUpgradeCost, incubatorUpgradeCost;
+    public Vector3 sawmillCostMultiplicator, mineCostMultiplicator, headquarterCostMultiplicator, incubatorCostMultiplicator;
+
+    private void Start()
+    {
+        sawmill = new Building { buildingName = sawmillName, cost = sawmillCost, upgradeCost = sawmillUpgradeCost, CostMultiplicator = sawmillCostMultiplicator };
+        mine = new Building { buildingName = mineName, cost = mineCost, upgradeCost = mineUpgradeCost, CostMultiplicator = mineCostMultiplicator };
+        headquarter = new Building { buildingName = headquarterName, cost = headquarterCost, upgradeCost = headquarterUpgradeCost, CostMultiplicator = headquarterCostMultiplicator };
+        incubator = new Building { buildingName = incubatorName, cost = incubatorCost, upgradeCost = incubatorUpgradeCost, CostMultiplicator = incubatorCostMultiplicator };
+    }
+
+    public void Upgrade(Building buildingToUpgrade)
+    {
+        if (buildingToUpgrade.level=)
+        {
+
+        }
+    }
+}
