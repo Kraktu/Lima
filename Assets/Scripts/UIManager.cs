d4fe23a2f36be89a0f6b31a3965822e20dacc30f
Strauss16 <normanandry@hotmail.be>
2020-01-14 17:03:59 +0100

    text on screen

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+	static public UIManager Instance { get; private set; }
+	public Text woodNumberText;
+	public Text oreNumberText;
+	public Text woodPerSecText;
+	public Text orePerSecText;
+	private void Awake()
+	{
+		if (Instance != null && Instance != this)
+		{
+			Destroy(gameObject);
+			return;
+		}
+
+		Instance = this;
+	}
+
+	private void Update()
+	{
+		woodNumberText.text = ResourceManager.Instance.wood.totalResource.ToString() + " woods";
+		oreNumberText.text = ResourceManager.Instance.ore.totalResource.ToString() + " ores";
+		woodPerSecText.text = ResourceManager.Instance.wood.resourcePerSecond.ToString();
+		orePerSecText.text = ResourceManager.Instance.ore.resourcePerSecond.ToString();
+
+	}
+}
