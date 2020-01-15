d2c12081038cbfe9a306aa129e37d49fd9501030
Strauss16 <normanandry@hotmail.be>
2020-01-15 16:21:39 +0100

    correction

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -32,11 +32,11 @@ public class UIManager : MonoBehaviour
 		orePerSecText.text = ResourceManager.Instance.ore.resourcePerSec.ToString("0") + " ores/sec";
 	}
 
-	public void TextMineUpdate(Vector3Int cost)
+	public void TextMineUpdate(Vector3 cost)
 	{
 		priceMineForUpgrade.text = cost.x.ToString("0") + " woods\n" + cost.y.ToString("0") + " ores\n" + cost.z.ToString("0") + " venacids";
 	}
-	public void TextSawmillUpdate(Vector3Int cost)
+	public void TextSawmillUpdate(Vector3 cost)
 	{
 		priceSawmillForUpgrade.text = cost.x.ToString("0") + " woods\n" + cost.y.ToString("0") + " ores\n" + cost.z.ToString("0") + " venacids";
 	}
