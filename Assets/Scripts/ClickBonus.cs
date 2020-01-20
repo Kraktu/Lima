a8a5f69f821f4fb28aa2c413d801f180a7683853
Strauss16 <normanandry@hotmail.be>
2020-01-20 13:55:27 +0100

    scene "p2w" : bonus sans spawn aléatoire

---
+++ Assets/Scripts/ClickBonus.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickBonus : MonoBehaviour
+{
+	void OnMouseDown()
+	{
+		ResourceManager.Instance.wood.totalResource += 200;
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/SpawnBonus.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBonus : MonoBehaviour
+{
+	public Transform[] SpawnChest;
+	public GameObject[] Bonus;
+	public int spawnTime = 1;
+
+    void Start()
+    {
+		InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Update()
+    {
+        
+    }
+
+	void Spawn()
+	{
+		for (int i = 0; i < Bonus.Length; i++)
+		{
+			int spawnIndex = Random.Range(0, spawnTime);
+			Instantiate(Bonus[i], transform.position, SpawnChest[spawnIndex].rotation);
+		}
+	}
+	
+}
