13f26ed85557b1b592342049a298cbfe0a83cca6
Strauss16 <normanandry@hotmail.be>
2020-01-21 11:59:26 +0100

    double onglet

---
+++ Assets/Scripts/SecondTab.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondTab : MonoBehaviour
+{
+	public GameObject menuToActivate, menuToDeactivate;
+
+	public void OpenPanel()
+	{
+		menuToActivate.SetActive(true);
+		menuToDeactivate.SetActive(false);
+	}
+}
