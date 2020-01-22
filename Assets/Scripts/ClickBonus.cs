7b3c1c2e1a365e7c48e44989690c117987bd5234
Strauss16 <normanandry@hotmail.be>
2020-01-22 09:30:37 +0100

    gems destroy

--- Assets/Scripts/ClickBonus.cs
+++ Assets/Scripts/ClickBonus.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class ClickBonus : MonoBehaviour
 {
+	public float time = 5;
+
+
+	private void Start()
+	{
+		Invoke("DestroyChest", time);
+	}
+
+	void DestroyChest()
+	{
+		Destroy(gameObject);
+	}
+
 	void OnMouseDown()
 	{
 		ResourceManager.Instance.gems.totalResource += 200;
