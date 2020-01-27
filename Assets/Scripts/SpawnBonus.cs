b352ff148a5f7ede93a6af74a754ac79a7c26e71
Strauss16 <normanandry@hotmail.be>
2020-01-27 14:33:29 +0100

    prefabs bonus + différents Bonus

--- Assets/Scripts/SpawnBonus.cs
+++ Assets/Scripts/SpawnBonus.cs
@@ -4,28 +4,45 @@ using UnityEngine;
 
 public class SpawnBonus : MonoBehaviour
 {
-	public int spawnMinTime = 1;
-	public int spawnMaxTime = 1;
+	public float spawnMinTime = 1;
+	public float spawnMaxTime = 1;
 
 	public Vector3 center;
 	public Vector3 size;
 
 	
-	public Transform[] SpawnChest;
-	public GameObject[] Bonus;
+	public ClickBonus[] Bonus;
+
+	float totalSpawnChance;
 
     void Start()
     {
 		Invoke("RandomSpawn", Random.Range(spawnMinTime, spawnMaxTime));
+		for (int i = 0; i <Bonus.Length; i++)
+		{
+			totalSpawnChance+= Bonus[i].spawnChance;
+		}
     }
 
 
 
 	public void RandomSpawn()
 	{
+		int generatedNumber = Random.Range(0, (int)totalSpawnChance + 1);
+		int objectToSpawn=0;
+		int passedNumber=0;
+		for (int i = 0; i < Bonus.Length; i++)
+		{
+			passedNumber += (int)Bonus[i].spawnChance;
+			if (generatedNumber<=passedNumber)
+			{
+				objectToSpawn = i;
+				break;
+			}
+		}
 		Vector3 posistion = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), 0, Random.Range(-size.z / 2, size.z / 2));
-		Instantiate(Bonus[0], posistion, Quaternion.identity);
-		int spawnTime = Random.Range(spawnMinTime, spawnMaxTime);
+		Instantiate(Bonus[objectToSpawn], posistion, Quaternion.identity);
+		float spawnTime = Random.Range(spawnMinTime, spawnMaxTime);
 		Invoke("RandomSpawn", spawnTime);
 
 	}
