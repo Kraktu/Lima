8b6200ea60e8d40d2f47a4dca2c0386b8e28d622
Strauss16 <57345327+Strauss16@users.noreply.github.com>
2020-01-28 10:34:09 +0100

    refinery comp points

--- Assets/Scripts/Refinery.cs
+++ Assets/Scripts/Refinery.cs
@@ -29,6 +29,46 @@ public class Refinery : ResourceBuilding
 			RefreshInterface();
 		}
 	}
+	public override void AddFirstSkillPoint()
+	{
+		base.AddFirstSkillPoint();
+		if (skillFirstUpgraded)
+		{
+			ResourceManager.Instance.percentVenacidBonusPerSec += skillFirstBonus;
+			UpdateRefineryProducing();
+			RefreshInterface();
+		}
+	}
+	public override void AddSecondSkillPoint()
+	{
+		base.AddSecondSkillPoint();
+		if (skillSecondUpgraded)
+		{
+			ResourceManager.Instance.flatVenacidBonusPerSec += skillSecondBonus;
+			UpdateRefineryProducing();
+			RefreshInterface();
+		}
+	}
+	public override void AddThirdSkillPoint()
+	{
+		base.AddThirdSkillPoint();
+		if (skillThirdUpgraded)
+		{
+			ResourceManager.Instance.percentVenacidBonusPerClick += skillThirdBonus;
+			UpdateRefineryProducing();
+			RefreshInterface();
+		}
+	}
+	public override void AddFourthSkillPoint()
+	{
+		base.AddFourthSkillPoint();
+		if (skillFourthUpgraded)
+		{
+			ResourceManager.Instance.flatVenacidBonusPerClick += skillFourthBonus;
+			UpdateRefineryProducing();
+			RefreshInterface();
+		}
+	}
 	public void UpdateRefineryProducing()
 	{
 		ClickProducingUpdate(ResourceManager.Instance.venacid, ResourceManager.Instance.startingVenacidPerClick, ResourceManager.Instance.percentVenacidBonusPerClick, ResourceManager.Instance.flatVenacidBonusPerClick);
@@ -40,6 +80,6 @@ public class Refinery : ResourceBuilding
 		_perClickString = producedResource + ": " + ResourceManager.Instance.venacid.resourcePerClick.ToString("0") + " /Click";
 		_perSecString = producedResource + ": " + (3600 * ResourceManager.Instance.venacid.resourcePerSec).ToString("0") + " /h";
 		UIManager.Instance.BuildingInterfaceUpdate(buildingNamePlusLevel, buildingDescription, currentCost, _perSecString, _perClickString, villagers, workerIconBuilding, buildingIcon, skillPoints.ToString() + " skill points",
-		firstSkillPointUpgradeName + skillFirstBonus.ToString("0") + "%" + " lvl." + firstSkillPointLevel, secondSkillPointUpgradeName + (skillSecondBonus * 3600).ToString("0") + " ores/h" + " lvl." + secondSkillPointLevel, thirdSkillPointUpgradeName + skillThirdBonus.ToString("0") + "%" + " lvl." + thirdSkillPointLevel, fourthSkillPointUpgradeName + skillFourthBonus.ToString("0") + " venacid/click" + " lvl" + fourthSkillPointLevel);
+		firstSkillPointUpgradeName + skillFirstBonus.ToString("0") + "%" + " lvl." + firstSkillPointLevel, secondSkillPointUpgradeName + (skillSecondBonus * 3600).ToString("0") + " venacid/h" + " lvl." + secondSkillPointLevel, thirdSkillPointUpgradeName + skillThirdBonus.ToString("0") + "%" + " lvl." + thirdSkillPointLevel, fourthSkillPointUpgradeName + skillFourthBonus.ToString("0") + " venacid/click" + " lvl" + fourthSkillPointLevel);
 	}
 }
