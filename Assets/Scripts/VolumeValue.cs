dca83f3ffaf3b1b137fab023d0c02539bb44c790
Strauss16 <normanandry@hotmail.be>
2020-02-04 14:40:48 +0100

    volume settings

---
+++ Assets/Scripts/VolumeValue.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeValue : MonoBehaviour
+{
+	AudioSource audioSrc;
+	float musicVolume = 1;
+    void Start()
+    {
+		audioSrc = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+		audioSrc.volume = musicVolume;
+    }
+
+	public void SetVolume(float vol)
+	{
+		musicVolume = vol;
+	}
+}
