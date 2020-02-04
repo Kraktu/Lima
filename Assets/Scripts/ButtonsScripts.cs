1241ef39426fca47a3d7a8d49d236b13a3c1de04
Strauss16 <normanandry@hotmail.be>
2020-02-04 13:56:48 +0100

    scripts button for settings etc, new test scene with buttons working

--- Assets/Scripts/ButtonsScripts.cs
+++ Assets/Scripts/ButtonsScripts.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MainMenu : MonoBehaviour
+public class ButtonsScripts : MonoBehaviour
 {
 	public GameObject panelOption;
 	public Button closePanel;
