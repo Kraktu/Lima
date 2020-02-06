89c2c0ad94d22bc8219b4c6e0e80197ab6aa2fbc
Kraktu <dupontphilip15@gmail.com>
2020-02-06 10:03:06 +0100

    TielScript 50%

--- Assets/Scripts/TileScript.cs
+++ Assets/Scripts/TileScript.cs
@@ -5,17 +5,73 @@ using UnityEngine;
 public class TileScript : MonoBehaviour
 {
 
-	[HideInInspector]
-	public int tileLvl, tilelvlMax, tileSkillPointNeeded;
+	public int tilelvlMax, tileSkillPointNeeded;
 	public bool visible, buyable, bought;
-	public Material invisibleMat, visibleMat, buyableMat,boughtMat,maxedMat;
+	public Material inexistantMat,invisibleMat, visibleMat, buyableMat,boughtMat,maxedMat;
 	public string tileName, tileDescription;
     public TileScript[] tileToSee, tileToUnlock;
 
+    [HideInInspector]
+    public int tileLvl;
+
+    [HideInInspector]
+    public Material myMat;
+    public void Start()
+    {
+        MyMatUpdate();
+    }
+
+    public void MyMatUpdate()
+    {
+        myMat = gameObject.GetComponent<MeshRenderer>().material;
+        if (tilelvlMax==0)
+        {
+            myMat = inexistantMat;
+        }
+        else if (!visible)
+        {
+            myMat = invisibleMat;
+        }
+        else if (!buyable)
+        {
+            myMat = visibleMat;
+        }
+        else if (!bought)
+        {
+            myMat = buyableMat;
+        }
+        else if (tileLvl != tilelvlMax)
+        {
+            myMat = boughtMat;
+        }
+        else
+        {
+            myMat = maxedMat;
+        }
+    }
+    public void SetMeVisible()
+    {
+
+    }
+    public void SetMeBuyable()
+    {
+
+    }
+    public void SetMeBought()
+    {
+
+    }
+    public void SetMeMaxed()
+    {
+
+    }
     public void OnMouseDown()
 	{
-        if (true)
+        if (buyable&&tileLvl!=tilelvlMax&&ResourceManager.Instance.skillPoint>0)
         {
+            ResourceManager.Instance.skillPoint++;
+            tileLvl++;
+            MyMatUpdate();
 
         }
 	}
@@ -26,13 +82,20 @@ public class TileScript : MonoBehaviour
 	}
 	public void OnMouseEnter()
 	{
-		UIManager.Instance.spherierPanel.SetActive(true);
-		UIManager.Instance.tileName.text = tileName;
-		UIManager.Instance.tileLvl.text = tileLvl.ToString("0") + " / " + tilelvlMax.ToString("0");
-		UIManager.Instance.tileDescription.text = tileDescription;
+        if (visible)
+        {
+            UIManager.Instance.spherierPanel.SetActive(true);
+            UIManager.Instance.tileName.text = tileName;
+            UIManager.Instance.tileLvl.text = tileLvl.ToString("0") + " / " + tilelvlMax.ToString("0");
+            UIManager.Instance.tileDescription.text = tileDescription;
+        }
+
 	}
 	public void OnMouseExit()
-	{
-		UIManager.Instance.spherierPanel.SetActive(false);
+    {
+        if (visible)
+        {
+            UIManager.Instance.spherierPanel.SetActive(false);
+        }
 	}
 }
