5526039d7759083ceda6e4b5302aefff52ae8d43
Kraktu <dupontphilip15@gmail.com>
2020-02-06 10:36:43 +0100

    TileScript 100%

--- Assets/Scripts/TileScript.cs
+++ Assets/Scripts/TileScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TileScript : MonoBehaviour
 {
@@ -10,6 +11,9 @@ public class TileScript : MonoBehaviour
 	public Material inexistantMat,invisibleMat, visibleMat, buyableMat,boughtMat,maxedMat;
 	public string tileName, tileDescription;
     public TileScript[] tileToSee, tileToUnlock;
+
+    public UnityEvent functionToCall;
+
 
     public int tileLvl;
 
@@ -50,30 +54,52 @@ public class TileScript : MonoBehaviour
     }
     public void SetMeVisible()
     {
-
+        if (!visible)
+        {
+            visible = true;
+            MyMatUpdate();
+        }
     }
     public void SetMeBuyable()
     {
-
+        if (visible&&!buyable)
+        {
+            buyable = true;
+            MyMatUpdate();
+        }
     }
     public void SetMeBought()
     {
-
-    }
-    public void SetMeMaxed()
-    {
-
+        if (buyable&&tileLvl>0)
+        {
+            bought = true;
+            MyMatUpdate();
+        }
     }
     public void OnMouseDown()
 	{
-        if (buyable&&tileLvl!=tilelvlMax&&ResourceManager.Instance.skillPoint>0)
+        if (buyable&&tileLvl<tilelvlMax&&ResourceManager.Instance.skillPoint>0)
         {
             ResourceManager.Instance.skillPoint++;
             tileLvl++;
+            SetMeBought();
             MyMatUpdate();
+            functionToCall.Invoke();
 
         }
 	}
+
+    public void ActivateAdjacentTile()
+    {
+        for (int i = 0; i < tileToSee.Length; i++)
+        {
+            tileToSee[i].SetMeVisible();
+        }
+        for (int i = 0; i < tileToUnlock.Length; i++)
+        {
+            tileToUnlock[i].SetMeBuyable();
+        }
+    }
 
 	public void OnMouseOver()
 	{
